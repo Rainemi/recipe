efb21524376a0af6e3e0dd7d72ba4ebe155dde8a
Rainemi <105657663+Rainemi@users.noreply.github.com>
2022-09-24 10:16:14 +0100

    Update urls.py

--- recipe/urls.py
+++ recipe/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_recipe.urls')),
+    path('', include('user.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
